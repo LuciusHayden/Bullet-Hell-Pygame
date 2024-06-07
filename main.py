285868d8bce2fd93e21c549e7ffb43dd45cf8dd2
Lucius Hayden <luciushaydenrocks@gmail.com>
2024-06-07 11:10:37 -0700

    Added comments

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ while run:
     run_time = pygame.time.get_ticks()
     
     screen.fill((0, 0, 0)) 
-
+#Draws needed text
     live_lost_text = font.render(f'You have lost a live', True, (255, 0, 0))
     health_text = font.render(f'Health: {player.health}', True, (255, 0, 0))
     lives_text = font.render(f'Lives: {player.lives}', True, (255, 0, 0))
@@ -133,7 +133,7 @@ while run:
     screen.blit(timer_text, (10, 10))
     screen.blit(lives_text, (screen_width - lives_text.get_width() - 200, 10))
 
-
+#Handles not taking damage to quickly after taking damage
     if not damage_cd and not life_cd:
         for enemy in enemies:
             if player.character.colliderect(enemy.character):
@@ -153,17 +153,17 @@ while run:
     if damage_cd:
         if pygame.time.get_ticks() - cd_start > 200:
             damage_cd = False
-
+#Handles losing lives
     if player.health <= 0:
         player.lives -= 1
         player.health = health
         lost_start_time = pygame.time.get_ticks()
         display_lost_life = True
-        
+#Prints the text for losing life      
     if display_lost_life == True:
         screen.blit(live_lost_text, (screen_width // 2 - live_lost_text.get_width() // 2, screen_height // 2 - live_lost_text.get_height() // 2))
         life_cd = True
-        
+#Handles stopping the text for losing life after 3 seconds or if your game is over    
     if display_lost_life:
         if player.lives == 0:
             life_cd = False
@@ -173,7 +173,8 @@ while run:
             life_cd = False
 
    
-
+#Handles the projectiles
+#multiple projectile types at the same time from all enemies
     for enemy in enemies:
         if run_time % 1000 == 0:
             projectile = Projectile(enemy.character.x, enemy.character.y, 15,10, 0.3, (player.character.x, player.character.y))
@@ -201,8 +202,10 @@ while run:
         if run_time > 90000 and run_time % 1000 == 0:
             projectile = Projectile(enemy.character.x, enemy.character.y, 10,5, 0.8, (player.character.x, player.character.y))
             projectiles5.add(projectile)
+#handles showing the player and the projectiles which is useful for other functions
     if show_player == True:
         pygame.draw.rect(screen, (0, 255, 0), player.character)
+# updates the projectile locations and draws them into the screen ONLY if show_projectiles is true
     if show_projectiles == True:
         projectiles.update()
         projectiles2.update()
@@ -220,6 +223,7 @@ while run:
         for projectile in projectiles5:
             pygame.draw.rect(screen, (100, 255, 255), projectile.character)
 
+#handles the movement of the player
     key = pygame.key.get_pressed()
     if key[pygame.K_a]:
         player.character.move_ip(-1 , 0)
@@ -229,14 +233,14 @@ while run:
         player.character.move_ip(0, -1)
     if key[pygame.K_s]:
         player.character.move_ip(0, 1)
-    
+#Draws the enemies and handles their movement
     for enemy in enemies:
         pygame.draw.rect(screen , (255, 0, 0), enemy.character)
         if enemy.direction in ["up", "down"]:
             enemy.move_up_down()
         elif enemy.direction in ["left", "right"]:
             enemy.move_left_right()
-    
+#Handles the game ending
     if player.lives <= 0:
         game_end()
     for event in pygame.event.get():
